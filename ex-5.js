3a359947028580608f27e5033b01ab67a13cdb86
jin <passawit53@gmail.com>
2025-03-05 14:33:04 +0700

    finished

--- ex-5.js
+++ ex-5.js
@@ -7,6 +7,8 @@ const students = [
 
 function getAverageStudentScore(students) {
   // Start coding here
+  return students.reduce((acc,students) => acc + students.score , 0)/students.length;
+
 }
 
-getAverageStudentScore(students); // Output: 87.5
+console.log (getAverageStudentScore(students)); // Output: 87.5
